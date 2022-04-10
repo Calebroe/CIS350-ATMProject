aa9849ec04193ea5b84f64da5034537e050c8812
Calebroe <100630232+Calebroe@users.noreply.github.com>
2022-04-10 16:32:49 -0400

    Update User.java

--- src/atm/User.java
+++ src/atm/User.java
@@ -1,13 +1,7 @@
 package atm;
 
-import java.awt.EventQueue;
+import java.util.Date;
 
-/*************************************************
- * Class for a User
- *
- * @author Caleb Roe
- * @version March 2, 2022
- *************************************************/
 public class User {
 	public int pin;
     public String firstName;
@@ -15,28 +9,14 @@ public class User {
     public int totalBalance;
     public int accountNumber;
     
-    /***********************************************************************
-	 * Constructor that initializes User
-	 * 
-	 * @param firstName     string of first name 
-	 * @param lastName      string of last name 
-	 * @param pin           integer of pin 
-	 * @param totalBalance  integer of total balance 
-	 * @param accountNumber integer of account number 
-	 ***********************************************************************/
-    public User(String firstName, String lastName, int pin, int totalBalance, int accountNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.pin = pin;
-        this.totalBalance = totalBalance;
-        this.accountNumber = accountNumber;
-    }
+    //public User(String firstName, String lastName, int pin, int totalBalance, int accountNumber) {
+    //    this.firstName = firstName;
+    //    this.lastName = lastName;
+    //    this.pin = pin;
+    //    this.totalBalance = totalBalance;
+    //    this.accountNumber = accountNumber;
+    //}
     
-    /***********************************************************************
-	 * Method that sets pin of User
-	 * @param pin integer of pin
-	 * @throws IllegalArgumentException()
-	 ***********************************************************************/
     public void setPin(int pin) {
         int length = String.valueOf(pin).length();
         if (length != 4) {
@@ -45,61 +25,45 @@ public class User {
         this.pin = pin;
     }
     
-    /***********************************************************************
-	 * Method that returns pin of User
-	 * @return pin
-	 ***********************************************************************/
     public int getPin() {
         return pin;
     }
     
-    /***********************************************************************
-   	 * Method that sets firstName of User
-   	 * @param firstName string of first name
-   	 * @throws IllegalArgumentException()
-   	 ***********************************************************************/
     public void setFirstName(String firstName) {
-        int length = String.valueOf(firstName).length();
-        if (firstName == null || length <= 0) {
+        int d = Integer.parseInt(firstName);
+        int length = String.valueOf(d).length();
+        if (firstName == null || firstName.length() == 0) {
+            throw new IllegalArgumentException();
+        }
+        if (length >= 0) {
+            throw new IllegalArgumentException();
+        }
+        if (firstName == null) {
             throw new IllegalArgumentException();
         }
         this.firstName = firstName;
     }
     
-    /***********************************************************************
-	 * Method that returns firstName of User
-	 * @return firstName
-	 ***********************************************************************/
     public String getFirstName() {
         return firstName;
     }
     
-    /***********************************************************************
-   	 * Method that sets last name of User
-   	 * @param lastName string of last name
-   	 * @throws IllegalArgumentException()
-   	 ***********************************************************************/
     public void setLastName(String lastName) {
-        int length = String.valueOf(lastName).length();
-        if (lastName == null || lastName.length() <= 0) {
+        int d = Integer.parseInt(lastName);
+        int length = String.valueOf(d).length();
+        if (lastName == null || lastName.length() == 0) {
+            throw new IllegalArgumentException();
+        }
+        if (length >= 0) {
             throw new IllegalArgumentException();
         }
         this.lastName = lastName;
     }
     
-    /***********************************************************************
-	 * Method that returns lastName of User
-	 * @return lastName
-	 ***********************************************************************/
     public String getLastName() {
         return lastName;
     }
     
-    /***********************************************************************
-   	 * Method that sets total balance of User
-   	 * @param totalBalance integer of balance
-   	 * @throws IllegalArgumentException()
-   	 ***********************************************************************/
     public void setTotalBalance(int totalBalance) {
         if (totalBalance < 0) {
             throw new IllegalArgumentException();
@@ -107,19 +71,10 @@ public class User {
         this.totalBalance = totalBalance;
     }
     
-    /***********************************************************************
-	 * Method that returns totalBalance of User
-	 * @return totalBalance
-	 ***********************************************************************/
     public int getTotalBalance() {
         return totalBalance;
     }
     
-    /***********************************************************************
-   	 * Method that sets account number of User
-   	 * @param accountNumber integer of balance
-   	 * @throws IllegalArgumentException()
-   	 ***********************************************************************/
     public void setAccountNumber(int accountNumber) {
         int length = String.valueOf(accountNumber).length();
         if (length != 10) {
@@ -128,10 +83,6 @@ public class User {
         this.accountNumber = accountNumber;
     }
     
-    /***********************************************************************
-	 * Method that returns account number of User
-	 * @return accountNumber
-	 ***********************************************************************/
     public int getAccountNumber() {
         return accountNumber;
     }
@@ -157,9 +108,12 @@ public class User {
         return false;
     }
 
-    public static void main(String[] args) {
-    	//User user1 = new User("Caleb", "Roe", 1111, 1400, 1001);
-    }
+  //toString()
+    @Override
+    public String toString() {
+        return "User [userId=" + userId + ", userName=" + userName + ", dob="
+                + dob + ", balance=" + balance + "accountNumber:" + accountNumber]";"
+    } 
 }
 	
 
